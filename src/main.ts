8bac9c0b540256482147c1624b522210292e5c9a
ufotablee <ufotablee@gmail.com>
2024-01-06 20:05:41 +0200

    first swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder().setTitle('Block list').build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
