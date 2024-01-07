0917bc6fba843b6eb5441efaf5815b0d0f3a05e2
ufotablee <ufotablee@gmail.com>
2024-01-07 19:39:15 +0200

    feat: account s/c/m

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { DbService } from './db/db.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DbModule } from './db/db.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, DbModule],
+  imports: [AuthModule, UsersModule, DbModule, AccountModule],
   controllers: [AppController],
   providers: [AppService, DbService],
   exports: [DbService],
